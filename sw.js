460db28c651403b14e36d84bb7222173ae0da40d
dario.zubaray <dzubaray@imagosur.com>
2019-10-25 09:19:35 -0300

    repaso, configurando el service worker

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_STATIC = 'static-v1';
+const CACHE_DYNAMIC = 'synamic-v1';
+const CACHE_INMUTABLE = 'inmutable-v1';
+
+const APP_SHELL = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    '/js/ap.js'
+];
+
+const APP_SHELL_INMUTABLE = [
+    'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+    'https://fonts.googleapis.com/css?family=Lato:400,300',
+    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    'css/animate.css',
+    'js/libs/jquery.js'
+];
+
+self.addEventListener('install', e => {
+
+    const staticCache = caches.open(CACHE_STATIC).then(cache => {
+        cache.addAll(APP_SHELL);
+    });
+
+    const inmutableCache = caches.open(CACHE_INMUTABLE).then(cache => {
+        cache.addAll(APP_SHELL_INMUTABLE);
+    });
+
+    e.waitUntill(Promise.all([staticCache, inmutableCache]));
+});
+
+self.addEventListener('activate', e => {
+
+    const borrarCache = caches.keys().then(keys => {
+        keys.forEach(key => {
+            if (key != CACHE_STATIC && keys.includes('static-')) {
+                return caches.delete(key);
+            }
+        });
+    });
+
+    e.waitUntil(borrarCache);
+})
